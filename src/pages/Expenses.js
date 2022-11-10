8e4491595d59f7fcf78d0c17501ad3b8df1e3d2f
chithranjan kumar <kumarchithranjan@gmail.com>
2022-11-10 19:53:36 +0530

    added feature to edit and delete the expenses

--- src/pages/Expenses.js
+++ src/pages/Expenses.js
@@ -12,8 +12,8 @@ const Expenses = () => {
 
   const email = JSON.parse(localStorage.getItem('idToken')).email;
   const emailUrl = email.replace(/[@.]/g, '');
-  // console.log(emailUrl);
 
+  // adding new expenses
   const addExpenseHandler = async (event) => {
     event.preventDefault();
 
@@ -35,17 +35,24 @@ const Expenses = () => {
 
       const data = await res.json();
       if (res.ok) {
+        const newData = {
+          amount: amountRef.current.value,
+          type: typeRef.current.value,
+          description: descriptionRef.current.value,
+        };
         setExpenseList((preState) => {
           const updatedList = [
             ...preState,
             {
-              amount: amountRef.current.value,
-              type: typeRef.current.value,
-              description: descriptionRef.current.value,
+              id: data.name,
+              ...newData,
             },
           ];
           return updatedList;
         });
+        amountRef.current.value = '';
+        typeRef.current.value = '';
+        descriptionRef.current.value = '';
       } else {
         throw data.error;
       }
@@ -54,6 +61,7 @@ const Expenses = () => {
     }
   };
 
+  // showing expenses when page is refreshed
   useEffect(() => {
     const getItems = async () => {
       try {
@@ -65,8 +73,8 @@ const Expenses = () => {
         if (res.ok) {
           const retrievedData = [];
 
-          for (let item in data) {
-            retrievedData.push(data[item]);
+          for (let key in data) {
+            retrievedData.push({ id: key, ...data[key] });
           }
           setExpenseList(retrievedData);
         } else {
@@ -79,10 +87,38 @@ const Expenses = () => {
     getItems();
   }, [emailUrl]);
 
+  // editing the expense
+  const edit = (item) => {
+    setExpenseList((preState) => {
+      const updatedItemList = preState.filter((data) => data.id !== item.id);
+      return updatedItemList;
+    });
+
+    amountRef.current.value = item.amount;
+    typeRef.current.value = item.type;
+    descriptionRef.current.value = item.description;
+  };
+
+  // deleting the expense
+  const deleted = (id) => {
+    setExpenseList((preState) => {
+      const updatedItemList = preState.filter((data) => data.id !== id);
+      return updatedItemList;
+    });
+  };
+
+  // mapping the expenses
   const newExpenseList = expenseList.map((item) => (
-    <ExpenseItems item={item} key={Math.random().toString()} />
+    <ExpenseItems
+      item={item}
+      key={item.id}
+      edit={edit}
+      deleted={deleted}
+      emailUrl={emailUrl}
+    />
   ));
 
+  // returning the component
   return (
     <React.Fragment>
       <form className={classes.form} onSubmit={addExpenseHandler}>
@@ -111,9 +147,9 @@ const Expenses = () => {
       {expenseList.length > 0 && (
         <div className={classes.items}>
           <div className={classes.title}>
-            <span>Type</span>
-            <span>Amount</span>
-            <span>Description</span>
+            <span className={classes.titletype}>Type</span>
+            <span className={classes.titleamount}>Amount</span>
+            <span className={classes.titledescription}>Description</span>
           </div>
           {newExpenseList}
         </div>
